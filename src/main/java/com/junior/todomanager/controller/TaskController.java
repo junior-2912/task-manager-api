c512dfac99e8ee9baba4ba04842768edc7a527ce
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-09-22 16:43:21 -0300

    feat: implements initial GET methods

--- src/main/java/com/junior/todomanager/controller/TaskController.java
+++ src/main/java/com/junior/todomanager/controller/TaskController.java
@@ -1,8 +1,23 @@
 package com.junior.todomanager.controller;
 
+import com.junior.todomanager.domain.Task;
+import com.junior.todomanager.services.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/tasks")
 @RestController
 public class TaskController {
+    private final TaskService taskService;
 
+    @GetMapping
+    public ResponseEntity<List<Task>> findAll() {
+        return ResponseEntity.ok(taskService.findAll());
+    }
 }
--- src/main/java/com/junior/todomanager/services/TaskService.java
+++ src/main/java/com/junior/todomanager/services/TaskService.java
@@ -3,9 +3,11 @@ package com.junior.todomanager.services;
 import com.junior.todomanager.domain.Task;
 import com.junior.todomanager.exceptions.ResourceNotFoundException;
 import com.junior.todomanager.repository.TaskRespository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskService {
     private final TaskRespository taskRespository;
 
