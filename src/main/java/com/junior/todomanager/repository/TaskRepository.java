a4a0557ca4995b112c52a417bf6c4e9912e359c3
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-09-22 17:11:59 -0300

    fix: fix name

--- src/main/java/com/junior/todomanager/repository/TaskRepository.java
+++ src/main/java/com/junior/todomanager/repository/TaskRepository.java
@@ -3,5 +3,5 @@ package com.junior.todomanager.repository;
 import com.junior.todomanager.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRespository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
