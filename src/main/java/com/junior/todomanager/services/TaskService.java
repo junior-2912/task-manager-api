ecae3a8d94cdfa2eb26159d3a0d11e7ae941eff8
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-09-22 17:12:09 -0300

    fix: fix name

--- src/main/java/com/junior/todomanager/services/TaskService.java
+++ src/main/java/com/junior/todomanager/services/TaskService.java
@@ -2,24 +2,24 @@ package com.junior.todomanager.services;
 
 import com.junior.todomanager.domain.Task;
 import com.junior.todomanager.exceptions.ResourceNotFoundException;
-import com.junior.todomanager.repository.TaskRespository;
+import com.junior.todomanager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TaskService {
-    private final TaskRespository taskRespository;
+    private final TaskRepository taskRepository;
 
-    public TaskService(TaskRespository taskRespository) {
-        this.taskRespository = taskRespository;
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
     }
 
     public List<Task> findAll() {
-        return taskRespository.findAll();
+        return taskRepository.findAll();
     }
 
     public Task findById(Long id) {
-        return taskRespository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task not found!"));
+        return taskRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task not found!"));
     }
 }
