c16d7a86403dd0df6bb27cb494059b64b41a7727
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-09-23 13:45:44 -0300

    feat: implements dto for task request

---
+++ src/main/java/com/junior/todomanager/dto/TaskRequestDto.java
@@ -0,0 +1,23 @@
+package com.junior.todomanager.dto;
+
+import com.junior.todomanager.enums.TaskCategory;
+import com.junior.todomanager.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class TaskRequestDto {
+    @NotBlank
+    private String title;
+    private String description;
+    @NotNull
+    private TaskStatus taskStatus;
+    private LocalDate dueDate;
+    @NotNull
+    private TaskCategory taskCategory;
+}
