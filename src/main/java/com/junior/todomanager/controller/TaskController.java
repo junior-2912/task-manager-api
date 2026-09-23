982a4a0dbbbc9de039a22c9eb4d827328b02658e
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-09-23 13:45:13 -0300

    feat: implements save method

--- src/main/java/com/junior/todomanager/controller/TaskController.java
+++ src/main/java/com/junior/todomanager/controller/TaskController.java
@@ -1,23 +1,44 @@
 package com.junior.todomanager.controller;
 
 import com.junior.todomanager.domain.Task;
+import com.junior.todomanager.dto.TaskRequestDto;
 import com.junior.todomanager.services.TaskService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/tasks")
 @RestController
 public class TaskController {
-    private final TaskService taskService;
+    private final TaskService service;
 
     @GetMapping
     public ResponseEntity<List<Task>> findAll() {
-        return ResponseEntity.ok(taskService.findAll());
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> save(@Valid @RequestBody TaskRequestDto taskRequestDto) {
+        Task task = service.save(taskRequestDto);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(task.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(task);
     }
 }
