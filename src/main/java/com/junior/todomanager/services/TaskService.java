8fb68c8be8bccaa0f5b518c16bf843ae5153da17
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-09-23 13:45:19 -0300

    feat: implements save method

--- src/main/java/com/junior/todomanager/services/TaskService.java
+++ src/main/java/com/junior/todomanager/services/TaskService.java
@@ -1,10 +1,14 @@
 package com.junior.todomanager.services;
 
 import com.junior.todomanager.domain.Task;
+import com.junior.todomanager.dto.TaskRequestDto;
+import com.junior.todomanager.exceptions.DateInvalidException;
 import com.junior.todomanager.exceptions.ResourceNotFoundException;
 import com.junior.todomanager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -21,5 +25,21 @@ public class TaskService {
 
     public Task findById(Long id) {
         return taskRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task not found!"));
+    }
+
+    @Transactional
+    public Task save(TaskRequestDto taskRequestDto) {
+        if (taskRequestDto.getDueDate().isBefore(LocalDate.now())) {
+            throw new DateInvalidException("Due date cannot be earlier than today");
+        }
+        Task task = Task.builder()
+                .title(taskRequestDto.getTitle())
+                .description(taskRequestDto.getDescription())
+                .taskCategory(taskRequestDto.getTaskCategory())
+                .taskStatus(taskRequestDto.getTaskStatus())
+                .dueDate(taskRequestDto.getDueDate())
+                .build();
+
+        return taskRepository.save(task);
     }
 }
